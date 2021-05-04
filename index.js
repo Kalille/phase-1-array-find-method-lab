7338c1be67d4f4497c0b352067262c1253bffc1e
kalille Overton <>
2021-05-04 14:23:23 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 
+ function superbowlWin(array){
+   
+let found = array.find(value => value.result==='W');{
+if (found){
+    return found.year
+}
+// else{
+//     return
+// }
+}
+
+
+}
+
+
+ // for (const win of array){
+    //     if (win.result ==='W'){
+    //         return win.year
+    //     }
+       
+    //     }
+    // }
+
+
+// // else{
+// //     return array.year
+// }
+
+// else {
+//     return array.result
+// }
+// }
+// else {
+//     return false
+// }
+
+
+
+   
+
+
+
+//     let findChip = record.find()
+//     if (record.result === 'w'){
+// return findChip(record.year)
+//     }
+    
+// const found = arr.find(arr){
+//     return arr.result === 'W'
+// })
